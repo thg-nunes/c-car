d5026985be8c7675919b89feb57f0c0c51295b6e
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-14 16:08:39 -0300

    feature: rota de specification

---
+++ src/routes/specification.routes.ts
@@ -0,0 +1,18 @@
+import { Request, Response, Router } from 'express';
+import { SpecificationRepository } from '../modules/cars/repositories/specificationRepository';
+import { CreateSpecificationService } from '../modules/cars/services/createSpecificationService';
+
+const specificationRoutes = Router();
+const specificationRepository = new SpecificationRepository();
+
+specificationRoutes.post('/', (req: Request, res: Response) => {
+  const { name, description } = req.body;
+
+  const specification_repository = new CreateSpecificationService(specificationRepository);
+
+  specification_repository.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+export { specificationRoutes };
