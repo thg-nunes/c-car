05587f1adcdb8fb86ecfbb7317032b624fe40bc2
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-08 13:47:50 -0300

    feat: metodo para carregar categorias

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,6 +1,47 @@
+import fs from 'fs';
+import csvParser from 'csv-parser';
+
+import { IRepositorie } from '../../repositories/ICategoriesRepository';
+
+type ImportCategory = {
+  name: string;
+  description: string;
+};
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  constructor(private categoryRepository: IRepositorie) {}
+
+  loadingCategories(file: Express.Multer.File): Promise<ImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: ImportCategory[] = [];
+
+      const fileParsed = csvParser();
+
+      stream.pipe(fileParsed);
+
+      fileParsed
+        .on('data', async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories: ImportCategory[] = await this.loadingCategories(file);
+
+    console.log(categories);
   }
 }
 
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoryRepositorie } from '../../repositories/categoriesRepositorie';
 import { ImportCategoryController } from './importCategoryController';
 import { ImportCategoryUseCase } from './importCategoryUseCase';
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoryRepositorie = CategoryRepositorie.instance;
+const importCategoryUseCase = new ImportCategoryUseCase(categoryRepositorie);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export { importCategoryController };
