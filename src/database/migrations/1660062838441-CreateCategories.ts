bb7b4d27bdc3aea2a71da777bd6c48c8bf4ed278
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-09 15:19:42 -0300

    refactor: renomeia diretorio

--- src/database/migrations/1660062838441-CreateCategories.ts
+++ src/database/migrations/1660062838441-CreateCategories.ts
@@ -1,7 +1,6 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateCategories1660062838441 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -23,15 +22,14 @@ export class CreateCategories1660062838441 implements MigrationInterface {
           {
             name: 'created_at',
             type: 'timestamp',
-            default: 'now()'
-          }
-        ]
-      })
-    )
+            default: 'now()',
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('categories')
+    await queryRunner.dropTable('categories');
   }
-
 }
