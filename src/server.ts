42f141bf1a459e0d556047591b3db419f6a0ac9d
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-18 15:11:39 -0300

    feat: rota para criar usuario

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
 import { specificationRoutes } from './routes/specification.routes';
+import { usersRoutes } from './routes/users.routes';
 
 import './database';
 import './shared/container';
@@ -12,5 +13,6 @@ app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
 app.use('/specification', specificationRoutes);
+app.use('/users', usersRoutes);
 
 app.listen(3333, () => console.log('server runing on port 3333'));
