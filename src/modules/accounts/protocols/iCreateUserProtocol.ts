e58dbbcd2a05c2b509e14620e9c25854059ba502
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-25 16:25:15 -0300

    refactor: remove coluna nao usada

--- src/modules/accounts/protocols/iCreateUserProtocol.ts
+++ src/modules/accounts/protocols/iCreateUserProtocol.ts
@@ -2,7 +2,6 @@ import { User } from '../infra/typeorm/entities/user';
 
 export type ICreateUserDTO = {
   name: string;
-  username: string;
   password: string;
   email: string;
   driver_license: string;
--- src/shared/infra/typeorm/migrations/1660759959901-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1660759959901-CreateUsers.ts
@@ -15,11 +15,6 @@ export class CreateUsers1660759959901 implements MigrationInterface {
             type: 'varchar',
           },
           {
-            name: 'username',
-            type: 'varchar',
-            isUnique: true,
-          },
-          {
             name: 'password',
             type: 'varchar',
           },
