05eae9b86592e46d4a89bbf6966fe207ce04a004
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-26 15:56:02 -0300

    .

--- src/modules/cars/useCases/createSpecification/createSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/createSpecificationUseCase.ts
@@ -1,20 +1,29 @@
 import { AppError } from '../../../../shared/errors/AppError';
 import { ICarRepositorie } from '../../infra/protocols/iCarRepositorie';
+import { ISpecificationRepository } from '../../infra/protocols/iSpecificationRepository';
 
-type IRequest = {
+interface IRequest {
   car_id: string;
-};
+  name: string;
+  description: string;
+}
 
 class CreateSpecificationUseCase {
-  constructor(private carRepository: ICarRepositorie) {}
-  async execute({ car_id }: IRequest) {
-    const car = await this.carRepository.findById(car_id);
+  constructor(private carRepositorie: ICarRepositorie, specificationRepository: ISpecificationRepository) {}
 
-    if (!car) {
+  async execute({ car_id, name, description }: IRequest) {
+    const car_exists = await this.carRepositorie.findById(car_id);
+
+    if (!car_exists) {
       throw new AppError('Car does not exists!');
     }
 
-    await this.carRepository.create(car);
+    const specificationAlreadyExists = this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) throw new AppError('Specification already exists');
+
+    this.specificationRepository.create({ name, description });
   }
 }
+
 export { CreateSpecificationUseCase };
