f98896bb68f0dcd03dfdc54d637f90abfc1a72a3
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-26 12:44:24 -0300

    feat: criacao de categoria somente por admin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { CreateCategoryController } from '../../../../modules/cars/useCases/createCategory/createCategoryController';
+import { authUser } from '../../../middlewares/authUser';
+import { userIsAdmin } from '../../../middlewares/userAdmin';
 
 const categoriesRoutes = Router();
 
 const createCategoryController = new CreateCategoryController();
 
 categoriesRoutes.get('/', createCategoryController.getCategories);
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post('/', authUser, userIsAdmin, createCategoryController.handle);
 
 export { categoriesRoutes };
