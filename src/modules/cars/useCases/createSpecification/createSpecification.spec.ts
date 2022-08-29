55da739c89923293086b473c15ab731fd00b74e7
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-08-29 13:19:48 -0300

    test: correcao na logica para criar uma nova specification

--- src/modules/cars/useCases/createSpecification/createSpecification.spec.ts
+++ src/modules/cars/useCases/createSpecification/createSpecification.spec.ts
@@ -1,27 +1,50 @@
+import { AppError } from '../../../../shared/errors/AppError';
+import { SpecificationRepository } from '../../infra/typeorm/repositories/category/specificationRepository';
 import { CarRepositoryInMemory } from '../createCar/carRepositoryInMemory';
 import { CreateSpecificationUseCase } from './createSpecificationUseCase';
+import { SpecificationRepositoryInMemory } from './specificationRepositoryInMemory';
 
 let createSpecificationUseCase: CreateSpecificationUseCase;
 let carRepositorieInMemory: CarRepositoryInMemory;
+let specificationRepositoryInMemory: SpecificationRepositoryInMemory;
 
 describe('CreateSpecification clas', () => {
   beforeEach(() => {
     carRepositorieInMemory = new CarRepositoryInMemory();
-    createSpecificationUseCase = new CreateSpecificationUseCase(carRepositorieInMemory);
+    specificationRepositoryInMemory = new SpecificationRepositoryInMemory();
+
+    createSpecificationUseCase = new CreateSpecificationUseCase(
+      carRepositorieInMemory,
+      specificationRepositoryInMemory,
+    );
   });
 
-  it('shold create a new specification to car', async () => {
-    const car_1 = await carRepositorieInMemory.create({
+  it('shold throw an error if car or specification id does not exists', () => {
+    expect(async () => {
+      const car_id = '1221';
+      const specification_id = ['5821'];
+
+      await createSpecificationUseCase.execute({ car_id, specification_id });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('shold create an new car if car id exists', async () => {
+    const car = await carRepositorieInMemory.create({
       available: true,
-      brand: 'xxxxx',
+      brand: 'any_brand',
       category_id: 'any_id',
-      daily_rate: 150,
+      daily_rate: 12,
       description: 'any_description',
-      fine_amount: 50,
-      license_plate: 'any_license',
+      fine_amount: 123,
+      license_plate: 'any_license_plate',
       name: 'any_name',
+      id: 'any_id',
     });
 
-    await createSpecificationUseCase.execute({ car_id: car_1.id });
+    const specification_id = ['1234'];
+
+    await createSpecificationUseCase.execute({ car_id: car.id, specification_id });
+
+    await createSpecificationUseCase.execute({ car_id: car.id, specification_id });
   });
 });
