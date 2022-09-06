5f697b9ecbbbcfcb393be13fd3dc0c210f744a2a
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-06 15:19:10 -0300

    feat: rota para devolver um carro

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { CreateRentalUseCaseController } from '../../../../modules/rentals/usecase/createRental/createRentalUseCaseController';
+import { DevolutionRentalController } from '../../../../modules/rentals/usecase/devolutionRental/devolutionRentalController';
 import { authUser } from '../../../middlewares/authUser';
 import { userIsAdmin } from '../../../middlewares/userAdmin';
 
 const rentalRoutes = Router();
 
 const createRentalUseCaseController = new CreateRentalUseCaseController();
+const devolutionRentalUseCaseController = new DevolutionRentalController();
 
 rentalRoutes.post('/', authUser, userIsAdmin, createRentalUseCaseController.handle);
+rentalRoutes.post('/devolution/:id', authUser, devolutionRentalUseCaseController.handle);
 
 export { rentalRoutes };
