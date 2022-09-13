5d7aa52933fabac45fe9e92391f07ce8e8d561ee
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-13 13:19:17 -0300

    feat: rota para buscar alugueis

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { CreateRentalUseCaseController } from '../../../../modules/rentals/usecase/createRental/createRentalUseCaseController';
 import { DevolutionRentalController } from '../../../../modules/rentals/usecase/devolutionRental/devolutionRentalController';
+import { FindRentalsController } from '../../../../modules/rentals/usecase/findRentals/findRentalsUseController';
 import { authUser } from '../../../middlewares/authUser';
 import { userIsAdmin } from '../../../middlewares/userAdmin';
 
@@ -8,8 +9,10 @@ const rentalRoutes = Router();
 
 const createRentalUseCaseController = new CreateRentalUseCaseController();
 const devolutionRentalUseCaseController = new DevolutionRentalController();
+const findRentalsController = new FindRentalsController();
 
 rentalRoutes.post('/', authUser, userIsAdmin, createRentalUseCaseController.handle);
 rentalRoutes.post('/devolution/:id', authUser, devolutionRentalUseCaseController.handle);
+rentalRoutes.get('/user/:id', authUser, findRentalsController.handle);
 
 export { rentalRoutes };
