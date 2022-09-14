34095c25ae81ef3bc343997f8b5de051e750902b
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-14 13:58:22 -0300

    feat: entidade para criar token de usuario

---
+++ src/modules/accounts/infra/typeorm/entities/users_tokens.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { User } from './user';
+
+@Entity('users_tokens')
+class UsersTokens {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  refash_token: string;
+
+  @Column()
+  expires_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { UsersTokens };
