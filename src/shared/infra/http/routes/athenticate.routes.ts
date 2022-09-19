f5ae847aca49d2875a8d930e3aa3889cac7c3647
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-19 14:35:22 -0300

    feat: rota para gerar um novo refresh token

--- src/shared/infra/http/routes/athenticate.routes.ts
+++ src/shared/infra/http/routes/athenticate.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { RefreshTokenController } from '../../../../modules/accounts/useCase/refreshToken/refreshTokenController';
 import { AuthenticateController } from '../../../authenticate/athenticateController';
 
 const authenticateRoutes = Router();
 
 const authenticateController = new AuthenticateController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post('/authenticate', authenticateController.handle);
+authenticateRoutes.post('/refresh-token', refreshTokenController.handle);
 
 export { authenticateRoutes };
